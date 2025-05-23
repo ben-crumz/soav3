11b22c904a675eea2fba890dda7f94706d062202
ben-crumz <109879960+ben-crumz@users.noreply.github.com>
2025-05-22 23:19:34 -0700

    Added basic functionality for the selector color to change when the gallery changes

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
  function hideColor(){
-            const elements = document.querySelectorAll('[data-color="bnw"]');
-                elements.forEach(elements => {
-                    elements.style.display = 'block';
-            });
-            const color = document.querySelectorAll('[data-color="color"]');
-                color.forEach(color => {
-                    color.style.display = 'none';
-            });
-        }
-        function hideBNW(){
-            const elements = document.querySelectorAll('[data-color="color"]');
-                elements.forEach(elements => {
-                    elements.style.display = 'block';
-            });
-            const color = document.querySelectorAll('[data-color="bnw"]');
-                color.forEach(color => {
-                    color.style.display = 'none';
-            });
-        }
+    const elements = document.querySelectorAll('[data-color="bnw"]');
+    elements.forEach(elements => {
+            elements.style.display = 'block';
+
+            document.getElementById("active-gallery").style.color = "#E1128E";
+            document.getElementById("not-active-gallery").style.color = "black";
+
+    });
+    const color = document.querySelectorAll('[data-color="color"]');
+    color.forEach(color => {
+            color.style.display = 'none';
+
+            document.getElementById("active-gallery").style.color = "black";
+            document.getElementById("not-active-gallery").style.color = "#E1128E";
+    });
+
+
+}
+function hideBNW(){
+    const elements = document.querySelectorAll('[data-color="color"]');
+    elements.forEach(elements => {
+            elements.style.display = 'block';
+
+            document.getElementById("active-gallery").style.color = "black";
+            document.getElementById("not-active-gallery").style.color = "#E1128E";
+
+    });
+    const color = document.querySelectorAll('[data-color="bnw"]');
+    color.forEach(color => {
+            color.style.display = 'none';
+
+            document.getElementById("active-gallery").style.color = "#E1128E";
+            document.getElementById("not-active-gallery").style.color = "black";
+    });
+
+
+}
